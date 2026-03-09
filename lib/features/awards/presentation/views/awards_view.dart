26edefe5706c72832b1e106298dd4220dd15e586
RottenShadow <xlostunite@gmail.com>
2026-03-09 15:25:40 +0200

    added awards cubit as screen level in awards view

--- lib/features/awards/presentation/views/awards_view.dart
+++ lib/features/awards/presentation/views/awards_view.dart
@@ -3,7 +3,6 @@ import 'package:ailixir/features/awards/presentation/cubits/award_cubit.dart';
 import 'package:ailixir/features/awards/presentation/widgets/awards_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 
 class AwardsView extends StatelessWidget {
   static const routeName = '/awards';
@@ -17,35 +16,9 @@ class AwardsView extends StatelessWidget {
         title: Text("Awards"),
         backgroundColor: AppColors.slate1000,
       ),
-      body: BlocBuilder<AwardsCubit, AwardState>(
-        builder: (listener_context, state) {
-          if (state is AwardInitial) {
-            GetIt.I.get<AwardsCubit>().getAwards(query);
-            return Center();
-          } else if (state is AwardLoading) {
-            return Center(child: CircularProgressIndicator());
-          } else if (state is AwardSuccess) {
-            return AwardsViewBody(query: query, awards: state.awards);
-          } else {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "ERROR: FAILED TO FETCH AWARDS",
-                    style: TextStyle(color: AppColors.red800),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      GetIt.I.get<AwardsCubit>().getAwards(query);
-                    },
-                    icon: Icon(Icons.refresh),
-                  ),
-                ],
-              ),
-            );
-          }
-        },
+      body: BlocProvider(
+        create: (context) => AwardsCubit()..getTestAwards(query),
+        child: AwardsViewBody(),
       ),
     );
   }
