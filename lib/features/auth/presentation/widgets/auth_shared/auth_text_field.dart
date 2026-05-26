3381a2646664c32b411e92ea373cb0c9d6529ce9
RottenShadow <xlostunite@gmail.com>
2026-05-26 23:14:25 +0300

    added shared core auth widgets

--- lib/features/auth/presentation/widgets/auth_shared/auth_text_field.dart
+++ lib/features/auth/presentation/widgets/auth_shared/auth_text_field.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ailixir/core/themes/app_colors.dart';
 import 'package:ailixir/core/themes/app_text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AuthTextField extends StatelessWidget {
   final String label;
@@ -12,6 +12,9 @@ class AuthTextField extends StatelessWidget {
   final TextEditingController? controller;
   final String? Function(String?)? validator;
   final Widget? topTrailing;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+  final void Function(String)? onFieldSubmitted;
 
   const AuthTextField({
     super.key,
@@ -23,6 +26,9 @@ class AuthTextField extends StatelessWidget {
     this.controller,
     this.validator,
     this.topTrailing,
+    this.keyboardType,
+    this.textInputAction,
+    this.onFieldSubmitted,
   });
 
   @override
@@ -36,58 +42,66 @@ class AuthTextField extends StatelessWidget {
             Text(
               label.toUpperCase(),
               style: AppTextStyles.labelmedium.copyWith(
-                color: AppColors.authTextSecondary,
-                letterSpacing: 1.5,
-                fontWeight: FontWeight.bold,
-                fontSize: 12.sp,
+                color: AppColors.slate400,
+                letterSpacing: 1.4,
+                fontWeight: FontWeight.w600,
+                fontSize: 11.sp,
               ),
             ),
             if (topTrailing != null) topTrailing!,
           ],
         ),
-        SizedBox(height: 10.h),
+        SizedBox(height: 8.h),
         TextFormField(
           controller: controller,
           obscureText: obscureText,
           validator: validator,
+          keyboardType: keyboardType,
+          textInputAction: textInputAction,
+          onFieldSubmitted: onFieldSubmitted,
           style: AppTextStyles.bodymedium.copyWith(color: Colors.white),
           decoration: InputDecoration(
             hintText: hint,
             hintStyle: AppTextStyles.bodymedium.copyWith(
-              color: AppColors.authTextSecondary.withValues(alpha: 0.5),
+              color: AppColors.slate500,
             ),
             prefixIcon: prefixIcon != null
-                ? Icon(
-                    prefixIcon,
-                    color: AppColors.authTextSecondary,
-                    size: 20.sp,
-                  )
+                ? Icon(prefixIcon, color: AppColors.slate400, size: 18.sp)
                 : null,
             suffixIcon: suffixIcon,
             filled: true,
-            fillColor: AppColors.cardBackground,
+            // Slightly lighter than the card so fields pop
+            fillColor: AppColors.slate700.withValues(alpha: 0.45),
             contentPadding: EdgeInsets.symmetric(
               horizontal: 16.w,
-              vertical: 18.h,
+              vertical: 16.h,
             ),
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12.r),
-              borderSide: const BorderSide(color: AppColors.brandBorder),
+              borderRadius: BorderRadius.circular(10.r),
+              borderSide: BorderSide(
+                color: AppColors.slate600.withValues(alpha: 0.6),
+              ),
             ),
             enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12.r),
-              borderSide: const BorderSide(color: AppColors.brandBorder),
+              borderRadius: BorderRadius.circular(10.r),
+              borderSide: BorderSide(
+                color: AppColors.slate600.withValues(alpha: 0.6),
+              ),
             ),
             focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12.r),
+              borderRadius: BorderRadius.circular(10.r),
               borderSide: const BorderSide(
                 color: AppColors.brandBlue,
                 width: 1.5,
               ),
             ),
             errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12.r),
+              borderRadius: BorderRadius.circular(10.r),
               borderSide: const BorderSide(color: Colors.redAccent),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.r),
+              borderSide: const BorderSide(color: Colors.redAccent, width: 1.5),
             ),
           ),
         ),
