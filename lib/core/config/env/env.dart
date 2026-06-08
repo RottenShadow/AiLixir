4cbabbc62c30a151264f9215472c2891363637b7
RottenShadow <xlostunite@gmail.com>
2026-06-08 16:46:56 +0300

    update gitignore rules

---
+++ lib/core/config/env/env.dart
@@ -0,0 +1,15 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'GOOGLE_CLIENT_ID')
+  static const String googleClientId = _Env.googleClientId;
+
+  @EnviedField(varName: 'GOOGLE_CLIENT_SECRET')
+  static const String googleClientSecret = _Env.googleClientSecret;
+
+  @EnviedField(varName: 'HF_TOKEN')
+  static const String hfToken = _Env.hfToken;
+}
