5d543c504b3b0f46a1f347358406380f85e33998
RottenShadow <xlostunite@gmail.com>
2026-06-08 09:30:41 +0300

    added app feature flag

---
+++ lib/core/utils/app_feature_flag.dart
@@ -0,0 +1,16 @@
+abstract class AppFeatureFlag {
+  // if true, use fake data instead of API.
+  // if false, use API.
+
+  static const bool useFakeGeneration = false;
+  static const bool useFakeDocking = false;
+  static const bool useFakeHistory = false;
+  static const bool useFakeAdmet = true;
+  static const bool useFakeChemicalSearch = true;
+  static const bool useFakeDrugRepurposing = true;
+  static const bool useFakeChatbot = true;
+  static const bool useFakeAwards = true;
+  static const bool useFakeScientists = true;
+  static const bool useFakeNews = true;
+  static const bool useFakeProfile = true;
+}
