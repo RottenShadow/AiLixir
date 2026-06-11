b99dc140c71e71554fe2fce5f1949a06493ed731
RottenShadow <xlostunite@gmail.com>
2026-06-11 10:18:35 +0300

    forced google sign in online only

--- lib/features/auth/data/repos/social_auth_repo_impl.dart
+++ lib/features/auth/data/repos/social_auth_repo_impl.dart
@@ -41,8 +41,7 @@ class SocialAuthRepoImpl {
         );
         _isGoogleSigninInit = true;
       }
-      var auth =
-          await googleSignIn.signIn() ?? await googleSignIn.signInOnline();
+      var auth = await googleSignIn.signInOnline();
       log('auth: ${auth?.accessToken}');
       token = auth!.accessToken;
       log('google access token: $token');
